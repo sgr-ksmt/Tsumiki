1a52123eac5d2282bbbd7daf1b3bc9747ef7e0e5
sgr-ksmt <melodydance.k.s@gmail.com>
2017-01-29 18:58:32 +0900

    Removed Instantiatable, Reusable extensions

--- Sources/UIKit/UICollectionExtensions.swift
+++ Sources/UIKit/UICollectionExtensions.swift
@@ -9,51 +9,7 @@
 import Foundation
 import UIKit
 
-public extension UICollectionView {
-    public enum Register {
-        case `class`
-        case nib
-    }
-    
-    public enum Kind {
-        case header
-        case footer
-        var value: String {
-            switch self {
-            case .header: return UICollectionElementKindSectionHeader
-            case .footer: return UICollectionElementKindSectionFooter
-            }
-        }
-    }
-    
-    public func register<C: UICollectionViewCell>(_ type: Register, cellType: C.Type) where C: CellRegisterable {
-        switch type {
-        case .class:
-            register(cellType, forCellWithReuseIdentifier: cellType.cellIdentifier)
-        case .nib:
-            let nib = UINib.init(nibName: cellType.className, bundle: nil)
-            register(nib, forCellWithReuseIdentifier: cellType.cellIdentifier)
-        }
-    }
-    
-    public func register<V: UICollectionReusableView>(_ type: Register, viewType: V.Type, of kind: Kind) {
-        switch type {
-        case .class:
-            register(viewType, forSupplementaryViewOfKind: kind.value, withReuseIdentifier: viewType.className)
-        case .nib:
-            let nib = UINib.init(nibName: viewType.className, bundle: nil)
-            register(nib, forSupplementaryViewOfKind: kind.value, withReuseIdentifier: viewType.className)
-        }
-    }
-    
-    public func dequeue<C: UICollectionViewCell>(cellType: C.Type, for indexPath: IndexPath) -> C where C: CellRegisterable {
-        return dequeueReusableCell(withReuseIdentifier: cellType.cellIdentifier, for: indexPath) as! C
-    }
-    
-    public func dequeue<V: UICollectionReusableView>(viewType: V.Type, of kind: Kind, for indexPath: IndexPath) -> V {
-        return dequeueReusableSupplementaryView(ofKind: kind.value, withReuseIdentifier: viewType.className, for: indexPath) as! V
-    }
-    
+public extension UICollectionView {    
     public func cellForItem<C: UICollectionViewCell>(at indexPath: IndexPath) -> C? {
         return cellForItem(at: indexPath) as? C
     }
--- Sources/UIKit/UITableViewExtensions.swift
+++ Sources/UIKit/UITableViewExtensions.swift
@@ -10,39 +10,6 @@ import Foundation
 import UIKit
 
 public extension UITableView {
-    public enum Register {
-        case `class`
-        case nib
-    }
-    
-    public func register<C: UITableViewCell>(_ type: Register, cellType: C.Type) where C: CellRegisterable {
-        switch type {
-        case .class:
-            register(cellType, forCellReuseIdentifier: cellType.cellIdentifier)
-        case .nib:
-            let nib = UINib.init(nibName: cellType.className, bundle: nil)
-            register(nib, forCellReuseIdentifier: cellType.className)
-        }
-    }
-    
-    public func register<V: UITableViewHeaderFooterView>(_ type: Register, viewType: V.Type) {
-        switch type {
-        case .class:
-            register(viewType, forHeaderFooterViewReuseIdentifier: viewType.className)
-        case .nib:
-            let nib = UINib.init(nibName: viewType.className, bundle: nil)
-            register(nib, forHeaderFooterViewReuseIdentifier: viewType.className)
-        }
-    }
-    
-    public func dequeue<C: UITableViewCell>(cellType: C.Type, for indexPath: IndexPath) -> C where C: CellRegisterable {
-        return dequeueReusableCell(withIdentifier: cellType.cellIdentifier, for: indexPath) as! C
-    }
-    
-    public func dequeue<V: UITableViewHeaderFooterView>(viewType: V.Type) -> V {
-        return dequeueReusableHeaderFooterView(withIdentifier: viewType.className) as! V
-    }
-    
     public func cellForRow<C: UITableViewCell>(at indexPath: IndexPath) -> C? {
         return cellForRow(at: indexPath) as? C
     }
